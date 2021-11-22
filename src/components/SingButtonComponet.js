0df1b886f95c60f406ea1be9615fe742822cdff1
canoe <ehgus430880@gmail.com>
2021-11-22 22:19:42 +0900

    Add button component
    새로운 버튼 컴포넌트를 개발하였습니다. 기존 리액트 버튼 컴포넌트는 다양한 스타일 적용이 어렵기 때문에 향후 개발에도 사용할 수 있는 컴포넌트를 프로젝트에 추가합니다.

---
+++ src/components/SingButtonComponet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, StyleSheet, Pressable} from 'react-native';
+
+export default function SignButton(props) {
+  console.log(props);
+  const {onPress, title = 'Save', textStyle, buttonStyle} = props;
+  return (
+    <Pressable style={buttonStyle} onPress={onPress}>
+      <Text style={textStyle}>{title}</Text>
+    </Pressable>
+  );
+}
