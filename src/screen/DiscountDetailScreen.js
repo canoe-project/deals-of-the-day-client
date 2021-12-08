827952b6e628ca0b45b10de17e0cfe9169e77457
canoe <ehgus430880@gmail.com>
2021-12-08 10:59:34 +0900

    Update SignInScreen Style
    키보드가 올라가면 스타일이 뭉게지는 버그가 있어 스타일을 수정하여 해결했습니다.

--- src/screen/DiscountDetailScreen.js
+++ src/screen/DiscountDetailScreen.js
@@ -1,34 +1,12 @@
 import React, {useContext, useState, useEffect} from 'react';
-import {Pressable, SafeAreaView, View, Image, ScrollView} from 'react-native';
+import {ScrollView} from 'react-native';
 import {DetailItemCard} from '../components/ItemCardComponent';
-import {productState, productDispatch} from '../store/productStore';
-
-import {malllistDispatch, malllistState} from '../store/mailListStore';
+import {malllistDispatch, malllistState} from '../store/mallListStore';
 
 const DiscountDetailsScreen = ({route, navigation}) => {
   const {item} = route.params;
   const {mallFindAll} = malllistDispatch();
   const state = malllistState();
-  // const testShop = [
-  //   {
-  //     id: 0,
-  //     name: 'elevenst',
-  //     price: 105000,
-  //     link: 'https://www.11st.co.kr/products/3939679662',
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'coupang',
-  //     price: 99000,
-  //     link: 'https://www.coupang.com/vp/products/6080959457',
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'coupang',
-  //     price: 99000,
-  //     link: 'https://www.coupang.com/vp/products/6080959457',
-  //   },
-  // ];
 
   useEffect(async () => {
     await mallFindAll(item.pcode);
@@ -49,22 +27,3 @@ const DiscountDetailsScreen = ({route, navigation}) => {
 };
 
 export default DiscountDetailsScreen;
-// // const {item} = route.params;
-// // const {mallFindAll} = malllistDispatch();
-// // const state = malllistState();
-// // useEffect(async () => {
-// // await mallFindAll(item.pcode);
-// // }, []);
-
-// return (
-//   <ScrollView>
-//     <DetailItemCard
-//     // style={{flex: 1}}
-//     // productImage={{uri: `https:${item.img}`}}
-//     // productName={item.name}
-//     // productPrice={item.price}
-//     // save={0}
-//     // shopData={state}
-//     />
-//   </ScrollView>
-// );
